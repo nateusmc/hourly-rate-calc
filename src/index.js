835b1a1aa325e5497190cbf61160506d22642554
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-07 12:07:34 -0600

    added starter files from Glitch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RateCalculator from './components/RateCalculator';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RateCalculator />,
+  document.getElementById('root')
+);
 registerServiceWorker();
